e58e2b9dbb5b557e519ec87f8cfd2cb3e5e067ba
Sdub76 <nospam@waun.net>
2019-12-02 09:07:44 -0500

    Update WaterBot2.ino

--- WaterBot2/WaterBot2/WaterBot2.ino
+++ WaterBot2/WaterBot2/WaterBot2.ino
@@ -5,18 +5,33 @@
  */
 #include <FreqMeasure.h>
 
-#define togglepin 9
+#define WATER_LEVEL_0 20
+#define WATER_LEVEL_1 50
+#define WATER_LEVEL_2 100
+#define WATER_LEVEL_3 200
+#define WATER_LEVEL_4 400
+
+//PIN DEFINITIONS
+#define LED_LEVEL1 2
+#define LED_LEVEL2 3 
+#define LED_LEVEL3 4
+#define LED_LEVEL4 5
+#define SENSOR_LEVEL 8
 
 void setup() {
   Serial.begin(57600);
   FreqMeasure.begin();
-  pinMode(9,OUTPUT);
+  pinMode(LED_BUILTIN,OUTPUT);
+  pinMode(LED_LEVEL1,OUTPUT);
+  pinMode(LED_LEVEL2,OUTPUT);
+  pinMode(LED_LEVEL3,OUTPUT);
+  pinMode(LED_LEVEL4,OUTPUT);
 }
 
 double sum=0;
 int count=0;
 boolean toggle = 0;
-double blinkcount = 0;
+float frequency = 0;
 
 void loop() {
   if (FreqMeasure.available()) {
@@ -24,12 +39,32 @@ void loop() {
     sum = sum + FreqMeasure.read();
     count = count + 1;
     if (count > 30) {
-      float frequency = FreqMeasure.countToFrequency(sum / count);
+      frequency = FreqMeasure.countToFrequency(sum / count);
       Serial.println(frequency);
       sum = 0;
       count = 0;
       toggle = !toggle;
     }
+    digitalWrite(LED_BUILTIN,toggle);
+    if (frequency > (WATER_LEVEL_1*0.9)){
+      digitalWrite(LED_LEVEL1,true);
+    } else {
+      digitalWrite(LED_LEVEL1,false);
+    }
+    if (frequency > (WATER_LEVEL_2*0.9)){
+      digitalWrite(LED_LEVEL2,true);
+    } else {
+      digitalWrite(LED_LEVEL2,false);
+    }
+    if (frequency > (WATER_LEVEL_3*0.9)){
+      digitalWrite(LED_LEVEL3,true);
+    } else {
+      digitalWrite(LED_LEVEL3,false);
+    }
+    if (frequency > (WATER_LEVEL_4*0.9)){
+      digitalWrite(LED_LEVEL4,true);
+    } else {
+      digitalWrite(LED_LEVEL4,false);
+    }
   }
-  digitalWrite(togglepin,toggle);
 }
